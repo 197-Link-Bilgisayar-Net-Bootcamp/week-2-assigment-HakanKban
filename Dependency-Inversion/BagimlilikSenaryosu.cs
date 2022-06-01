56f28dd84c0838cb2a20c5a4a4de7b36136b58ce
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2022-06-01 11:41:00 +0300

    SOLID

---
+++ Dependency-Inversion/BagimlilikSenaryosu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    //High Level Class
+    class Imalat
+    {
+        //Kek sınıfındaki değşiklik Olustur metodunu etkliyecektir.
+        public void Olustur()
+        {
+            Kek kek = new Kek();
+            kek.KekYap(true);
+        }
+    }
+    //Low Level Class
+    class Kek
+    {
+        public void KekYap(bool Kakao)
+        {
+
+        }
+    }
+
+
+    internal class BagimlilikSenaryosu
+    {
+    }
+}
