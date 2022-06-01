c39d68ed1f080965e269235805e18728a7b9b53f
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2022-06-01 12:01:10 +0300

    SOLID tamamlandı

---
+++ Dependency-Inversion/BagimlilikTersSenaryosu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+    interface IGida
+    {
+        void Yap(bool kakao);
+    }
+    //High Level Class--- Referans olarak interface kulanddığından dolaylı olarak bağımlılık alt seviyeden üst seviyeye olmuştur.
+    class Imalatt
+    {
+        IGida gida;
+
+        public Imalatt(IGida gida)
+        {
+            this.gida = gida;
+        }
+
+        public void Olustur()
+        {
+            gida.Yap(false);
+        }
+    }
+    //Low Level Class --- Görüldüğü gibi alt seviye sınıf interface bağımlı hale gelmiştir.
+    class Kekk : IGida
+    {
+        public void Yap(bool kakao)
+        {
+
+        }
+    }
+    internal class BagimlilikTersSenaryosu
+    {
+    }
+}
