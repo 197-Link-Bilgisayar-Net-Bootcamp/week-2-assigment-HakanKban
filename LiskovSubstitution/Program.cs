4ca803af636be467effbc1bd0d39b78c4475e6ce
Hakan Kaban <hkaban12@gmail.com>
2022-06-01 14:20:17 +0300

    Delete LiskovSubstitution directory

--- LiskovSubstitution/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿/* Barbara Liskov’un ortaya attığı bu prensibe göre ;
- * üst sınıf ile alt sınıf arasında davranış olarak hiçbir fark olmamalıdır. 
- * Yani birbirlerinin yerine kullanılabilmelidirler.
- * Base classlar kalıtım veren sınıflar, derived classlardaki işlevselliği tam olarak yerine getirdiğinden emin olmalıyız.
- * Bir sınıfta bulunan herhangi bir işlev, kendisinden kalıtım alan sınıflarda kullanılmayacaksa eğer 
- * işte bu durum LSP’ye aykırı bir durumdur.
- */
