14677ac70e23ead4e66cbcc41e6ffae4a2f09c3d
Hakan Kaban <hkaban12@gmail.com>
2022-06-01 14:20:30 +0300

    Delete OpenClosed directory

--- OpenClosed/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿/*Yazılım projeleri sürekli değişime açıktır. İhtiyaçların değişmesi sürekli değişimi getirir.
-Yenilemeler var olanı bozmayacak,var olanda değişiklik yapmadan sadece ekleme ve
-değiştirme yapacağımız kısımla muhattap olacağız.
-Kısaca prensip sınıfların gelişmeye açık, değişmeye kapalı olmasıdır.
-*/
-
