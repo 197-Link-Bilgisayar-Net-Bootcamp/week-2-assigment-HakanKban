8b95cbf5085f9965ebacd6c73f7b7686543262fd
Hakan Kaban <hkaban12@gmail.com>
2022-06-01 14:20:03 +0300

    Delete InterfaceSegregation directory

--- InterfaceSegregation/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿// Interfaceler sınıfların anlaşmalarıdır. Haliyle bir sınıfın implement ettiği bir Interface zorunlu 
-// şekilde sınıfa uygulanır. Interface yetersiz ya da fazla ise dağıtmak gerekir. Bu prensipin mantığı budur.
